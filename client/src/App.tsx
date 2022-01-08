216993e35fb5a09e9f67c267bba70fc99db07da0
zivserphos <zivfromisrael@gmail.com>
2022-01-08 18:02:28 +0200

    added style for the navbar
    added routes for about, contact-us , sign-in , home  and styled them in navbar.scss
    also added pages folder with home.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./state/pages/Home";
 import NavBar from "./state/components/NavBar";
 
 const App = function () {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
