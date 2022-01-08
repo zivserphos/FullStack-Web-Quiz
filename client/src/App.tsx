860567ac5cd87d3f7d10f23eea1970bed9556c4d
zivserphos <zivfromisrael@gmail.com>
2022-01-08 18:09:46 +0200

    added some empty pages for all links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./state/pages/Home";
+import About from "./state/pages/About";
+import SignUp from "./state/pages/SignUp";
 import NavBar from "./state/components/NavBar";
+import Services from "./state/pages/Services";
+import ContactUs from "./state/pages/ContactUs";
 
 const App = function () {
   return (
@@ -10,6 +14,10 @@ const App = function () {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
