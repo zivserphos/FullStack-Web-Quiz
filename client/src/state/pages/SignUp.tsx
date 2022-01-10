59aed8c1d9490cabd2a14d7a3653701eb62969fa
zivserphos <zivfromisrael@gmail.com>
2022-01-10 08:48:05 +0200

    started to build my sign-up page

--- client/src/state/pages/SignUp.tsx
+++ client/src/state/pages/SignUp.tsx
@@ -1,16 +1,74 @@
 import React from "react";
+import "./styles/sign-up.scss";
 
 const SignUp = function () {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "90vh",
-        alignItems: "center",
-      }}
-    >
-      <h1>Enjoy the freeom</h1>
+    <div className="sign-up">
+      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
+      <div className="container" id="container">
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href="/sign-up" className="social">
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a href="/sign-up" className="social">
+                <i className="fab fa-google-plus-g" />
+              </a>
+              <a href="/sign-up" className="social">
+                <i className="fab fa-linkedin-in" />
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="button">Sign Up</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign in</h1>
+            <div className="social-container">
+              <a href="/sign-up" className="social">
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a href="/sign-up" className="social">
+                <i className="fab fa-google-plus-g" />
+              </a>
+              <a href="/sign-up" className="social">
+                <i className="fab fa-linkedin-in" />
+              </a>
+            </div>
+            <span>or use your account</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="/sign-up">Forgot your password?</a>
+            <button type="button">Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button type="button" className="ghost" id="signIn">
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button type="button" className="ghost" id="signUp">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
