9147a9e656cdc74b676de470f28e5eaaa03abb69
zivserphos <zivfromisrael@gmail.com>
2022-01-16 20:05:32 +0200

    finished build grid component

--- client/src/components/Grid/Grid.tsx
+++ client/src/components/Grid/Grid.tsx
@@ -1,15 +1,43 @@
 /* eslint-disable react/require-default-props */
 import React, { FunctionComponent, ReactNode } from "react";
 import cn from "classnames";
-import "./grid.scss";
+import styles from "./grid.module.scss";
 
 const Grid: FunctionComponent<{
   children: ReactNode;
   item?: boolean;
   container?: boolean;
-  cols?: Cols;
-}> = function ({ children, container, item, cols }) {
-  return <div className="grid">{children}</div>;
+  xs?: Cols;
+  sm?: Cols;
+  md?: Cols;
+  lg?: Cols;
+  spacing?: Spacing;
+  alignItems?: AlignItems;
+  justifyContent: JustifyContent;
+}> = function ({
+  children,
+  container,
+  item,
+  xs,
+  sm,
+  md,
+  lg,
+  alignItems,
+  spacing,
+  justifyContent,
+}) {
+  const classNames = cn({
+    [styles.Grid_container]: container,
+    [styles.Grid_item]: item,
+    [styles[`Grid_xs_${xs}`]]: xs,
+    [styles[`Grid_xs_${sm}`]]: sm,
+    [styles[`Grid_xs_${md}`]]: md,
+    [styles[`Grid_xs_${lg}`]]: lg,
+    [styles[`Grid_spacing_${spacing}`]]: spacing,
+    [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
+    [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
+  });
+  return <div className={classNames}>{children}</div>;
 };
 
 export default Grid;
