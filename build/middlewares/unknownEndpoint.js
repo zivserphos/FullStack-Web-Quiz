2dbcf54523d487474ca880c93a84d7a3a51bc1f6
zivserphos <zivfromisrael@gmail.com>
2022-02-15 07:57:28 +0200

    .

--- build/middlewares/unknownEndpoint.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const unknownEndPoint = (_req, res, next) => {
-    res.redirect("/");
-    // res.status(404).json({ error: "Unknown endpoint" });
-    next();
-};
-exports.default = unknownEndPoint;
