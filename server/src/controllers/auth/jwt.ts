01bc302ae1577a7069db245af6898dbf6e974628
zivserphos <zivfromisrael@gmail.com>
2022-02-16 13:55:21 +0200

    edited jwt

--- server/src/controllers/auth/jwt.ts
+++ server/src/controllers/auth/jwt.ts
@@ -1,12 +1,35 @@
+/* eslint-disable no-underscore-dangle */
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { Handler } from "express";
+import User from "../../db/models/User";
+import config from "../../utils/config/index";
 
-const logout: Handler = (req, res) => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  req.session = null!;
-  req.logout();
-  res.redirect("/sign-up");
+// const logout: Handler = async (req, res) => {};
+
+const login: Handler = async (req, res) => {
+  const { userName, password } = req.body;
+  const user = await User.findOne({ userName });
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.hashPassword);
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: "invalid username or password",
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, config.secret);
+
+  return res
+    .status(200)
+    .send({ token, username: user.username, name: user.name, id: user._id })
+    .redirect("/");
 };
 
-const signIn: Handler = (_req, res) => res.redirect("/");
-
-export default { logout, signIn };
+export default login;
