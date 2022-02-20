976b86f060350c4193bb5bb5cd2e921cc81fc6ec
zivserphos <zivfromisrael@gmail.com>
2022-02-20 12:59:31 +0200

    build linkein scraper for jobs

--- src/services/scraper.ts
+++ src/services/scraper.ts
@@ -1,53 +1,56 @@
-/* eslint-disable no-shadow */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 import cheerio from "cheerio";
-import config from "../utils/config";
 
-const scrapeImg = ($: cheerio.Root, elem: cheerio.Element) => {
-  const logo = $(elem).find("img");
-  // console.log(logo.attr("src"));
-  // .find("div")
-  // .first()
-  // .find("div")
-  // .first()
-  // .find("img")
-  // .attr("src");
-  // console.log(logo);
-};
+const genUrlToscrape = (jobTitle: string, location: string, start = 0) =>
+  `https://www.linkedin.com/jobs/search?keywords=${jobTitle.replace(
+    " ",
+    "+"
+  )}&location=${location
+    .replace(" ", "+")
+    .replace(
+      ",",
+      "%2C"
+    )}&geoId?trk=homepage-jobseeker_job-search-link&start=${start}`;
 
-const scrapeTags = ($: cheerio.Root, elem: cheerio.Element) => {
-  const tags: string[] = [];
+const scrapeImg = ($: cheerio.Root, elem: cheerio.Element) =>
   $(elem)
-    .find("h3")
-    .siblings("div")
+    .find(".search-entity-media")
     .children()
-    .each((_, tagElem) => tags.push($(tagElem).text()));
-  console.log(tags);
-};
+    .first()
+    .attr("data-delayed-url") || "";
 
-const scrapeHeadLine = ($: cheerio.Root, elem: cheerio.Element) => {
-  const headLine = $(elem).find("h2");
-  // console.log(headLine.text());
-};
+const scrapeHeadLine = ($: cheerio.Root, elem: cheerio.Element) =>
+  $(elem).find("h3").text().trim();
 
-const scrapeFromJob = ($: cheerio.Root, elem: cheerio.Element) => {
-  const img = scrapeImg($, elem);
-  const headLine = scrapeHeadLine($, elem);
-  const tags = scrapeTags($, elem);
-};
+const scrapeLocation = ($: cheerio.Root, elem: cheerio.Element) =>
+  $(elem).find(".job-search-card__location").text().trim();
 
-const mainScraper = async () => {
-  const res = await axios.get(config.jobsUrl);
+const scrapeCompanyName = ($: cheerio.Root, elem: cheerio.Element) =>
+  $(elem).find("h4").text().trim();
 
+const scrapeCompanyInfo = ($: cheerio.Root, elem: cheerio.Element) =>
+  $(elem).find("a").attr("href") || "";
+
+const scrapeFromJob = ($: cheerio.Root, elem: cheerio.Element): Job => ({
+  img: scrapeImg($, elem),
+  headLine: scrapeHeadLine($, elem),
+  location: scrapeLocation($, elem),
+  companyName: scrapeCompanyName($, elem),
+  companyInfo: scrapeCompanyInfo($, elem),
+});
+
+const mainScraper = async (
+  jobTitle: string,
+  location: string,
+  start: number
+) => {
+  const res = await axios.get(genUrlToscrape(jobTitle, location, start));
   const $ = cheerio.load(res.data);
-  const jobs = $(".listResults > div").each((_, elem) =>
-    scrapeFromJob($, elem)
+  const jobs: Job[] = [];
+  $(".jobs-search__results-list > li > div ").each((_, elem) =>
+    jobs.push(scrapeFromJob($, elem))
   );
-  console.log(jobs.length);
+  return jobs;
 };
-
-mainScraper();
 
 export default mainScraper;
