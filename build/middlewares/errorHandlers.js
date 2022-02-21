24612bf82069d03be6d1b37c4039fe6edd9b5cb1
zivserphos <zivfromisrael@gmail.com>
2022-02-21 20:54:02 +0200

    unused files (for now)

--- build/middlewares/errorHandlers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, _req, res, 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-_next) => "status" in err
-    ? res.status(err.status).send(err.message)
-    : res.status(500).send("internal serverError");
-exports.default = errorHandler;
