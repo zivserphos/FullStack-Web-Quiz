44e1aee99492a312356b41aeddf5ef6c4681c1ac
zivserphos <zivfromisrael@gmail.com>
2022-02-21 20:54:02 +0200

    unused files (for now)

--- server/build/utils/config/jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    testEnvironment: "node",
-    roots: ["<rootDir>/tests"],
-    preset: "ts-jest",
-};
-exports.default = config;
