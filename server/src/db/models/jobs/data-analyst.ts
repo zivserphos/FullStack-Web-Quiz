47bfb2c2efdb9d0e4faef51df29b752e52bf2e87
zivserphos <zivfromisrael@gmail.com>
2022-04-03 11:19:08 +0300

    build a schema for each job at jobs router

---
+++ server/src/db/models/jobs/data-analyst.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+import mongoose, { Schema } from "mongoose";
+
+const DataAnalystSchema: Schema = new mongoose.Schema({
+  img: {
+    type: String,
+    required: true,
+  },
+  headLine: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+
+  companyName: {
+    type: String,
+    required: true,
+  },
+  companyInfo: {
+    type: String,
+  },
+});
+
+DataAnalystSchema.set("toJSON", {
+  transform: (_, returnedObject) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    returnedObject.id = <string>returnedObject._id.toString();
+    delete returnedObject._id;
+  },
+});
+
+const DataAnalyst = mongoose.model("jobs-dataAnalyst", DataAnalystSchema);
+export default DataAnalyst;
