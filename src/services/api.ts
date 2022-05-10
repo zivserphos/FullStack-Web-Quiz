d9c8a3fc059cc8a46cebf5bb0951816dc6b33a20
zivserphos <zivfromisrael@gmail.com>
2022-05-10 19:30:38 +0300

    build server

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-underscore-dangle */
 import fs from "fs";
-import { faker } from "@faker-js/faker";
 import QuizModel from "../db/models/Quiz";
 import User from "../db/models/User";
 
@@ -20,16 +19,6 @@ const sendQuiz = async (quiz: Quiz, userEmail: string) => {
   user.save();
 };
 
-const genFakeQuiz = (subject: string): FakeQuiz => ({
-  questions: genQuiz(subject),
-  subject,
-  date: faker.date.between(
-    "2022-01-01T00:00:00.000Z",
-    "2022-05-08T00:00:00.000Z"
-  ),
-  result: Math.random() > 0.82 ? 15 : Math.ceil(Math.random() * 14),
-});
-
 const genQuestions = (subject: string, limit: number) => {
   const contentFile = JSON.parse(
     fs.readFileSync(`./src/db/questions/${subject}.json`).toString()
@@ -45,22 +34,8 @@ const genQuestions = (subject: string, limit: number) => {
   return quiz;
 };
 
-const genFakeUser = (num: number) => {
-  const fakeUsers: FakeUser[] = [];
-  for (let i = 0; i < num; i += 1) {
-    fakeUsers.push({
-      date: faker.date.between(
-        "2022-05-01T00:00:00.000Z",
-        "2022-06-01T00:00:00.000Z"
-      ),
-      email: faker.internet.email(),
-    });
-  }
-  return fakeUsers;
-};
-
 const genQuiz = (subject: string) => genQuestions(subject, 15);
 const genCustom = (subject: string, limit: number) =>
   genQuestions(subject, limit);
 
-export default { genQuiz, genCustom, sendQuiz, genFakeQuiz, genFakeUser };
+export default { genQuiz, genCustom, sendQuiz };
