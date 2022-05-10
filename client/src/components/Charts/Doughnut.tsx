c66f7948b4c988f9b5f8f348eecc455344d69d44
zivserphos <zivfromisrael@gmail.com>
2022-05-10 17:56:37 +0300

    Added pie chart for display perfet quizzes amount by subject

--- client/src/components/Charts/Doughnut.tsx
+++ client/src/components/Charts/Doughnut.tsx
@@ -2,20 +2,35 @@
 // import axios from "axios";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Doughnut as DoughnutComponent } from "react-chartjs-2";
+import { Doughnut as DoughnutComponent, Pie } from "react-chartjs-2";
+import colors from "../../utils/colors";
 import "./styles/doughnut.scss";
 
-const dataForDoughnut = (data: number[]) => ({
+const dataForPie = (data: number[]) => ({
   labels: ["Perfect Quiz", "Not A Perfect Quiz"],
   datasets: [
     {
       label: "The ratio of perfect quizzes to those not",
       data,
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
+      backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+      hoverOffset: 4,
+    },
+    // {
+    //   label: "perfect quizzes by subject",
+    //   data,
+    //   backgroundColor: ["rgb(255, 205, 86)", "green"],
+    //   hoverOffset: 4,
+    // },
+  ],
+});
+
+const dataForDoughnut = (data: { [key: string]: number }) => ({
+  labels: Object.keys(data),
+  datasets: [
+    {
+      label: "perfect quizzes by subject",
+      data: Object.values(data),
+      backgroundColor: colors,
       hoverOffset: 4,
     },
   ],
@@ -30,6 +45,20 @@ const Doughnut = function () {
       0
     );
     return [perfectQuizzes, fakeQuizzes.length - perfectQuizzes];
+  };
+
+  const perfectQuizzesPerSubject = () => {
+    const subjects = fakeQuizzes
+      .filter((quiz) => quiz.result === 15)
+      .reduce(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (accu: any, quiz: FakeQuiz) =>
+          accu[quiz.subject]
+            ? { ...accu, [quiz.subject]: accu[quiz.subject] + 1 }
+            : { ...accu, [quiz.subject]: 1 },
+        {}
+      );
+    return subjects;
   };
   useEffect(() => {
     const fetchFakeQuizzes = async () => {
@@ -48,10 +77,21 @@ const Doughnut = function () {
   return (
     <div style={{ width: "40%", height: "30%" }}>
       {fakeQuizzes ? (
-        <DoughnutComponent
-          data={dataForDoughnut(ratioOfQuizzes())}
-          options={{ responsive: true, maintainAspectRatio: true }}
-        />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Pie
+            data={dataForPie(ratioOfQuizzes())}
+            options={{ responsive: true, maintainAspectRatio: true }}
+          />
+          <div>
+            <h1 style={{ textAlign: "right" }}>
+              Perfect Quizzes Divided Into Subjects
+            </h1>
+            <DoughnutComponent
+              data={dataForDoughnut(perfectQuizzesPerSubject())}
+              options={{ responsive: true, maintainAspectRatio: true }}
+            />
+          </div>
+        </div>
       ) : (
         "Currently Could Not Fetch Quizzes Data"
       )}
