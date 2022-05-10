fa638341fbd8bbb3695334d082d182c350d47505
zivserphos <zivfromisrael@gmail.com>
2022-05-10 19:56:02 +0300

    test

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.resolve("./client")));
 
+console.log(path.resolve("./client"));
+
 app.use("/auth", AuthRouter);
 app.use("/api", tokenExtractor, userExtractor, ApiRouter);
 app.use("/email", EmailRouter);
